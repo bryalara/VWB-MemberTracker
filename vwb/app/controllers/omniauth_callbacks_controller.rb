0ea38d078bbf2399dd0f64d280154937dd488754
MaoAnying <entao@tamu.edu>
2021-02-15 11:10:59 -0600

    google login

---
+++ vwb/app/controllers/omniauth_callbacks_controller.rb
@@ -0,0 +1,11 @@
+class OmniauthCallbacksController < ApplicationController
+    @user = User.from_google(request.env["omniauth.auth"])
+
+    if @user.persisted?
+        flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
+        sign_in_and_redirect @user, :event => :authentication
+    else
+        session["devise.user_data"] = request.env["omniauth.auth"]
+        redirect_to new_user_registration_path
+    end
+end
--- vwb/app/models/user.rb
+++ vwb/app/models/user.rb
@@ -1,6 +1,30 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  def self.from_google(access_token)
+    data = access_token.info
+    identify = Identify.find_by(:provider => access_token.provider, :uid => access_token.uid)
+
+    if identify
+        return identify.user
+    else
+        user = User.find_by(:email => access_token.email)
+        if !user
+            user = User.create(
+                email: data["email"],
+                password: Devise.friendly_token[0,20]
+            )
+        end
+            identify = Identify.create(
+                provider: access_token.provider,
+                uid: access_token.uid,
+                user: user
+            )
+        return user
+    end
+  end
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable
+  has_many :identifies
 end
