3c1a805193905b194a96baa1136c124c55ccf7da
sainikhilnaru <sainikhilnaru@gmail.com>
2021-02-27 17:34:58 -0600

    Updated Path

--- vwb/app/javascript/packs/application.js
+++ vwb/app/javascript/packs/application.js
@@ -10,7 +10,6 @@ require("channels")
 
 //= require jquery  
 //= require bootstrap-sprockets
-//= require member
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
