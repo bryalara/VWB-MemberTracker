282e3856a0622bc4274826a39558e9e15fe8adb1
Daniel Phan <phand972@gmail.com>
2021-02-28 17:20:37 -0600

    Reverted route changes and deleted imports in application.css

--- vwb/config/routes.rb
+++ vwb/config/routes.rb
@@ -1,30 +1,34 @@
 Rails.application.routes.draw do
-	root "pages#show", page: "home"
-	get "/:page" => "pages#show"
+  root "pages#show", page: "home"   # --- SNN
+  get "/pages/:page" => "pages#show"
+  resources :users do
+    collection {post :import} #used for csv importing
+  end
+  devise_for :userlogins, controllers: {omniauth_callbacks: "userlogins/omniauth_callbacks"}
+  devise_scope :userlogin do
+    get 'userlogins/sign_in', to: 'userlogins/sessions#new', as: :new_userlogin_session
+    get 'userlogins/sign_out', to: 'userlogins/sessions#destroy', as: :destroy_userlogin_session
+  end
 
-	resources :users do
-		collection {post :import} #used for csv importing
-	end
+  
+  # get 'home/index'
+  # root to: 'home#index'            ---SNN
+  # resources :home                   ---SNN
 
-	devise_for :userlogins, controllers: {omniauth_callbacks: "userlogins/omniauth_callbacks"}
-	devise_scope :userlogin do
-		get 'userlogins/sign_in', to: 'userlogins/sessions#new', as: :new_userlogin_session
-		get 'userlogins/sign_out', to: 'userlogins/sessions#destroy', as: :destroy_userlogin_session
-	end
 
-	get '/pendingApproval', to: 'users#pendingApproval'
-	get '/import', to: 'users#my_import'
-	# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/pendingApproval', to: 'users#pendingApproval'
+  get '/import', to: 'users#my_import'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-	resources :point_event do
-		member do
-			get :delete
-		end
-	end
+  resources :point_event do
+  	member do
+  		get :delete
+  	end
+  end
 
-	resources :event do
-		member do
-			get :delete
-		end
-	end
+  resources :event do
+  	member do
+  		get :delete
+  	end
+  end
 end
