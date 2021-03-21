51e530efaca2c680fa748cb08cd17057dc533de2
naru12 <naru12@tamu.edu>
2021-03-21 03:24:47 -0500

    Successfully updated home page through admin CRUD table

--- vwb/app/controllers/edithomepages_controller.rb
+++ vwb/app/controllers/edithomepages_controller.rb
@@ -1,4 +1,7 @@
 class EdithomepagesController < ApplicationController
+  protect_from_forgery with: :exception
+  before_action :authenticate_userlogin!
+
   def index
     @edithomepages = Edithomepage.all
   end
@@ -17,12 +20,25 @@ class EdithomepagesController < ApplicationController
 
   def create
     @edithomepage = Edithomepage.create(edithomepage_params)
-    redirect_to edithomepages_path
+    if @edithomepage.save
+      redirect_to edithomepages_path
+    else
+      render :new
+    end
   end
 
   def update
     @edithomepage = Edithomepage.find(params[:id])
-    @edithomepage.update(edithomepage_params)
+    
+    if @edithomepage.update(edithomepage_params)
+      redirect_to edithomepages_path
+    else 
+      render :edit
+    end
+  end
+
+  def delete
+    @edithomepage = Edithomepage.find(params[:id])
   end
 
   def destroy
--- vwb/db/seeds.rb
+++ vwb/db/seeds.rb
@@ -6,9 +6,14 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Edithomepage.create(name: 'Title0', description: 'This is the title0')
-Edithomepage.create(name: 'Title1', description: 'This is the title1')
-Edithomepage.create(name: 'Title2', description: 'This is the title2')
+Edithomepage.create(name: 'home', description: 'This is the home description')
+Edithomepage.create(name: 'about_us', description: 'This is the about us description')
+Edithomepage.create(name: 'contact_us', description: 'This is the contact us description')
+Edithomepage.create(name: 'officers', description: 'This is the officers description')
+Edithomepage.create(name: 'officer_image_url', description: 'This is the the officer image URL description')
+Edithomepage.create(name: 'landing_page_image_url', description: 'This is the landing page image url description')
+Edithomepage.create(name: 'events_info', description: 'This is the events info description')
+
 
 user = User.new(email: 'bryalara@tamu.edu', role: 1, firstName: 'Bryan', lastName: 'Lara', phoneNumber:'7138847570', classification:'Senior', tShirtSize:'XXL', optInEmail: true, participationPoints: 0, approved: true)
 
