ca8e37ec5444638095d96cccb7d3a9be2609e1ef
Bryan Lara <bryalara@tamu.edu>
2021-03-23 00:22:58 -0500

    Fixing member being redirected to root_path, fixed member page path on navbars, add download csv button in admin page, add navbar to member page and registration

--- vwb/app/controllers/application_controller.rb
+++ vwb/app/controllers/application_controller.rb
@@ -5,8 +5,8 @@ class ApplicationController < ActionController::Base
     authenticate_userlogin!
     if !User.exists?(email: current_userlogin.email)
       redirect_to registration_user_path
-    elsif User.find_by(email: current_userlogin.email).role.zero?
-      redirect_to root_path, notice: 'You are not an Admin'
+    # elsif User.find_by(email: current_userlogin.email).role.zero?
+    #   redirect_to root_path, notice: 'You are not an Admin'
     end
   end
 end
