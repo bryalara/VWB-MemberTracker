fc2f985a00f71d0af4f6fa8bb454c27c080f6491
Daniel Phan <phand972@gmail.com>
2021-03-30 01:46:11 -0500

    Testing again

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -85,22 +85,22 @@ end
 
 Capybara.server = :puma, { Silent: true }
 
-Capybara.register_driver :chrome do |app|
-  Capybara::Selenium::Driver.new(app, browser: :chrome)
-end
+# Capybara.register_driver :chrome do |app|
+#   Capybara::Selenium::Driver.new(app, browser: :chrome)
+# end
 
-Capybara.register_driver :headless_chrome do |app|
-  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-    chromeOptions: {
-      args:['no-sandbox', 'headless', 'disable-gpu', 'window-size=1280,800']
-    },
-  )
+# Capybara.register_driver :headless_chrome do |app|
+#   capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+#     chromeOptions: {
+#       args:['no-sandbox', 'headless', 'disable-gpu', 'window-size=1280,800']
+#     },
+#   )
 
-  Capybara::Selenium::Driver.new app,
-    browser: :chrome,
-    desired_capabilities: capabilities
-end
+#   Capybara::Selenium::Driver.new app,
+#     browser: :chrome,
+#     desired_capabilities: capabilities
+# end
 
-#Capybara.javascript_driver = :chrome
+# Capybara.javascript_driver = :chrome
 
-Capybara.default_driver = :chrome
+Capybara.default_driver = :selenium_chrome_headless
