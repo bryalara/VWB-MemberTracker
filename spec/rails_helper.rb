b5b4167d100ab300ffe6e7ebd59bae59695d83a8
Daniel Phan <phand972@gmail.com>
2021-03-30 00:33:39 -0500

    Uncommented settings in rails_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -85,21 +85,21 @@ end
 
 Capybara.server = :puma, { Silent: true }
 
-# Capybara.register_driver :chrome do |app|
-#   Capybara::Selenium::Driver.new(app, browser: :chrome)
-# end
+Capybara.register_driver :chrome do |app|
+  Capybara::Selenium::Driver.new(app, browser: :chrome)
+end
 
-# Capybara.register_driver :headless_chrome do |app|
-#   capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-#     chromeOptions: {
-#       args:['no-sandbox', 'headless', 'disable-gpu', 'window-size=1280,800']
-#     },
-#   )
+Capybara.register_driver :headless_chrome do |app|
+  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+    chromeOptions: {
+      args:['no-sandbox', 'headless', 'disable-gpu', 'window-size=1280,800']
+    },
+  )
 
-#   Capybara::Selenium::Driver.new app,
-#     browser: :chrome,
-#     desired_capabilities: capabilities
-# end
+  Capybara::Selenium::Driver.new app,
+    browser: :chrome,
+    desired_capabilities: capabilities
+end
 
 # Capybara.javascript_driver = :chrome
 
