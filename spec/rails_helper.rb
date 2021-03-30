c5807725e1736a5b66cfebe6b3e1da8e5151a6b1
Daniel Phan <phand972@gmail.com>
2021-03-29 21:37:05 -0500

    Adjusted tests to remove sleeps

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -78,29 +78,29 @@ RSpec.configure do |config|
 end
 
 
-require "selenium/webdriver"
-require "webdrivers/chromedriver"
+# require "selenium/webdriver"
+# require "webdrivers/chromedriver"
 
-Webdrivers::Chromedriver.required_version = "89.0.4389.90"
+# Webdrivers::Chromedriver.required_version = "89.0.4389.90"
 
 Capybara.server = :puma, { Silent: true }
 
-Capybara.register_driver :chrome do |app|
-  Capybara::Selenium::Driver.new(app, browser: :chrome)
-end
+# Capybara.register_driver :chrome do |app|
+#   Capybara::Selenium::Driver.new(app, browser: :chrome)
+# end
 
-Capybara.register_driver :headless_chrome do |app|
-  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-    chromeOptions: {
-      args:['no-sandbox', 'headless', 'disable-gpu', 'window-size=1280,800']
-    },
-  )
+# Capybara.register_driver :headless_chrome do |app|
+#   capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+#     chromeOptions: {
+#       args:['no-sandbox', 'headless', 'disable-gpu', 'window-size=1280,800']
+#     },
+#   )
 
-  Capybara::Selenium::Driver.new app,
-    browser: :chrome,
-    desired_capabilities: capabilities
-end
+#   Capybara::Selenium::Driver.new app,
+#     browser: :chrome,
+#     desired_capabilities: capabilities
+# end
 
-Capybara.javascript_driver = :headless_chrome
+#Capybara.javascript_driver = :headless_chrome
 
-# Capybara.default_driver = :selenium_chrome_headless
+Capybara.default_driver = :selenium_chrome
