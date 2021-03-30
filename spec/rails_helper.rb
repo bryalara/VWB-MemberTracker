5b4767923f350d4b9b6c111d8c7f3cc411362e09
Daniel Phan <phand972@gmail.com>
2021-03-30 01:15:53 -0500

    Use headless selenium

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -103,4 +103,4 @@ Capybara.server = :puma, { Silent: true }
 
 #Capybara.javascript_driver = :chrome
 
-Capybara.default_driver = :selenium_chrome
+Capybara.default_driver = :selenium_chrome_headless
