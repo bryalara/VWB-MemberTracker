9af3e541cb86975cf7188765741a6875204cf426
naru12 <naru12@tamu.edu>
2021-04-05 23:54:04 -0500

    added light upload image functionality

--- app/controllers/officers_controller.rb
+++ app/controllers/officers_controller.rb
@@ -1,11 +1,20 @@
 class OfficersController < ApplicationController
+  protect_from_forgery with: :exception
+  before_action :authenticate_userlogin!
+  
   def index
     @officers = Officer.all
   end
 
   def create 
-    officer = Officer.create(officer_params)
-    redirect_to_officers_path
+    @officer = Officer.create(officer_params)
+    @officer.image.attach(officer_params[:image])
+
+    if @officer.save
+      redirect_to officers_path
+    else
+      render :new
+    end
   end
 
 
@@ -14,6 +23,7 @@ class OfficersController < ApplicationController
   end
 
   def new
+    @officer = Officer.new
   end
 
   def edit
@@ -22,8 +32,18 @@ class OfficersController < ApplicationController
 
   def update
     @officer = Officer.find(params[:id])
-    @officer.update(officer_params)
-    redirect_to officer_path(@officer)
+    @officer.image.purge
+    @officer.image.attach(officer_params[:image])
+    if @officer.update(officer_params)
+      format.html { redirect_to officers_path, notice: 'Post was successfully updated.' }
+      redirect_to officers_path
+    else
+      render :edit
+    end
+  end
+
+  def delete
+    @officer = Officer.find(params[:id])
   end
 
   def destroy
@@ -35,6 +55,7 @@ class OfficersController < ApplicationController
 
 
   def officer_params
-    params.require(:officer).permit( :name, :email, :photoUrl, :description)
+    params.require(:officer).permit( :name, :email, :description,:image)
   end
+  
 end
--- app/models/officer.rb
+++ app/models/officer.rb
@@ -1,2 +1,3 @@
 class Officer < ApplicationRecord
+    has_one_attached :image
 end
