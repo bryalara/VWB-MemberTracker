e89a07f4fcad287f133bb5c5ec537c6616b72155
Yingtao Jiang <entao@tamu.edu>
2021-04-12 02:41:54 -0500

    fix some rubocop issues

--- app/models/point_event.rb
+++ app/models/point_event.rb
@@ -88,15 +88,15 @@ class PointEvent < ApplicationRecord
 
   # import csv
   def self.my_import_part(file)
-    events = []
+    # events = []
     wmsg = []
     begin
       CSV.foreach(file.path, headers: true) do |row|
         # puts('READING FROM CSV..........................................')
         # puts(row.to_h[1])
-        event_id, event_name, user_id, user_1, user_2, user_email, attended, created_at, updated_at = row
+        _event_id, event_name, _user_id, user_first, user_second, _user_email, attended, _created_at, _updated_at = row
         @event = PointEvent.find_by(name: event_name)
-        @user = User.find_by(id: user_id)
+        @user = User.find_by(firstName: user_first, lastName: user_second)
         if @event && @user
           unless @event.users.find_by(id: user_id)
             @event.users << @user
