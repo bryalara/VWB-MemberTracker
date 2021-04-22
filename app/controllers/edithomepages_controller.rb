71d2e62386068a7bcf9cc45407985668154a6027
naru12 <naru12@tamu.edu>
2021-04-21 20:05:20 -0500

    added h1 d1 h2 d2 fields and put them in index html

--- app/controllers/edithomepages_controller.rb
+++ app/controllers/edithomepages_controller.rb
@@ -53,7 +53,7 @@ class EdithomepagesController < ApplicationController
   private
 
   def edithomepage_params
-    params.require(:edithomepage).permit(:name, :description)
+    params.require(:edithomepage).permit(:name, :description, :h1, :d1, :h2, :d2)
   end
 
   def image_params
--- db/migrate/20210320101609_create_edithomepages.rb
+++ db/migrate/20210320101609_create_edithomepages.rb
@@ -3,6 +3,10 @@ class CreateEdithomepages < ActiveRecord::Migration[6.0]
     create_table :edithomepages, id: :uuid do |t|
       t.string :name
       t.string :description
+      t.string :h1
+      t.string :d1
+      t.string :h2
+      t.string :d2
 
       t.timestamps
     end
